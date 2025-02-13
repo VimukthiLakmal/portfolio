cb45561ce2a31311761e698d29c5e15fa985cf47
VimukthiLakmal <87938134+VimukthiLakmal@users.noreply.github.com>
2025-02-13 15:54:03 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/IIT-19-019 SE.pdf";
+import pdf from "../../Assets/../Assets/vimukthi lakmal.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
